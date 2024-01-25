6014a73e332c42645ba2f006219109cebec03feb
navid-al-azim <navidalazim95@gmail.com>
2024-01-25 17:39:23 +0600

    css, nav, footer modified, contents paraphrased on home page, added fontawsome for logos on footer, linked the review pages on homepage

--- js/footer.js
+++ js/footer.js
@@ -10,54 +10,57 @@ function loadFooter() {
   <div class="footer-column">
     <h4>CASINO ONLINE</h4>
     <ul>
-      <li><a href="#">Fast Pay</a></li>
-      <li><a href="#">Best Pay</a></li>
-      <li><a href="#">Bitcoin</a></li>
-      <li><a href="#">Neteller</a></li>
-      <li><a href="#">Debit Card</a></li>
+      <li><a href="/pages/casinos/fast-payout/">Fast Pay</a></li>
+      <li><a href="/pages/casinos/best-pay/">Best Pay</a></li>
+      <li><a href="/pages/casinos/payments/bitcoin/">Bitcoin</a></li>
+      <li><a href="/pages/casinos/payments/neteller/">Neteller</a></li>
+      <li><a href="/pages/casinos/payments/debit">Debit Card</a></li>
     </ul>
   </div>
 
   <div class="footer-column">
     <h4>REVIEWS</h4>
     <ul>
-      <li><a href="#">New Casino</a></li>
-      <li><a href="#">Android</a></li>
-      <li><a href="#">iPhone</a></li>
-      <li><a href="#">Real Money Casino</a></li>
+      <li><a href="/pages/reviews/new-casinos/">New Casino</a></li>
+      <li><a href="/pages/reviews/mobile-casinos/android/">Android</a></li>
+      <li><a href="/pages/reviews/mobile-casinos/iphone">iPhone</a></li>
+      <li><a href="/pages/reviews/real-money-casinos">Real Money Casino</a></li>
     </ul>
   </div>
 
   <div class="footer-column">
     <h4>CASINO GAMES</h4>
     <ul>
-      <li><a href="#">Blackjack</a></li>
-      <li><a href="#">Bingo</a></li>
-      <li><a href="#">Online Lottery</a></li>
+      <li><a href="/pages/games/blackjack">Blackjack</a></li>
+      <li><a href="/pages/games/bingo/">Bingo</a></li>
+      <li><a href="/pages/games/online-lottery/">Online Lottery</a></li>
     </ul>
   </div>
 
   <div class="footer-column">
     <h4>BONUSES</h4>
     <ul>
-      <li><a href="#">No Deposit</a></li>
-      <li><a href="#">Free Spin</a></li>
-      <li><a href="#">Cashback</a></li>
+      <li><a href="/pages/bonuses/no-deposit">No Deposit</a></li>
+      <li><a href="/pages/bonuses/free-spins/">Free Spin</a></li>
+      <li><a href="/pages/bonuses/cashback/">Cashback</a></li>
     </ul>
   </div>
 
   <div class="footer-column">
     <h4>FREE SLOTS</h4>
     <ul>
-      <li><a href="#">Popular Slots</a></li>
-      <li><a href="#">Real Money Slots</a></li>
+      <li><a href="/pages/free-slots/popular-slots/">Popular Slots</a></li>
+      <li><a href="/pages/free-slots/real-money-slots/">Real Money Slots</a></li>
     </ul>
   </div>
 
   <div class="footer-social">
-    <a href="#"><img src="facebook-logo.png" alt="Facebook Logo"></a>
-    <a href="#"><img src="instagram-logo.png" alt="Instagram Logo"></a>
-    <a href="#"><img src="email-logo.png" alt="Email Logo"></a>
+    <a href="#" class="fa fa-facebook"></a>
+    <a href="#" class="fa fa-instagram"></a>
+    <a href="#" class="fa fa-twitter"></a>
+    <a href="#" class="fa fa-linkedin"></a>
+    <a href="#" class="fa fa-telegram"></a>
+    <a href="#" class="fa fa-pinterest"></a>
   </div>
 </footer>
   `;
